fe9524388212536d8cd16da7ec47ec3fbe8a4ce4
unknown <dldlsry053@naver.com>
2022-09-10 01:32:14 +0900

    실습완료

--- Chapter4/Lecture6/lecture6.c
+++ Chapter4/Lecture6/lecture6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <limits.h> // 정수 범위 등 자료형의 최대 혹은 최소값을 기호적 상수로 표현합니다.
+#include <float.h> // FLT_MAX ....
 
 
 #define PI 3.141592	// manifest constants, symbolic constants
@@ -7,8 +8,9 @@
 int main()
 {
 	printf("PI is %f\n", PI);
-
-
+	printf("Biggest int is %d\n", INT_MAX);
+	printf("One byte in this system is %d bits\n", CHAR_BIT);
+	printf("Smallest normal float %e\n", FLT_MIN);
 
 	return 0;
 }
