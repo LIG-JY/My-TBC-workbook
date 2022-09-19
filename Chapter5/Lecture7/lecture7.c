63515fe9356666e527837585eccf78aefa9ef750
unknown <dldlsry053@naver.com>
2022-09-20 04:46:51 +0900

    실습완료

--- Chapter5/Lecture7/lecture7.c
+++ Chapter5/Lecture7/lecture7.c
@@ -3,16 +3,45 @@
 
 int main()
 {
-	//int seconds = 0, minutes = 0, hours = 0;
+	int seconds = 0, minutes = 0, hours = 0;
+	printf("Input seconds : "); 
+	scanf("%d", &seconds); // 처음 loop에 사용할 값
 
+	while (seconds >= 0)
+	{
+		
 
+		minutes = seconds / 60;
+		seconds = seconds % 60;
 
+		hours = minutes / 60;
+		minutes = minutes % 60;
 
-	//printf("Good bye\n");
+		printf("%d hours. %d minutes, %d seconds", hours, minutes, seconds);
 
-	//int div, mod;
+		printf("Input seconds : "); 
+		scanf("%d", &seconds); // 다음loop에 사용할 값
+	}
 	
+	printf("good bye\n");
 
+	int div, mod;
+
+	div = 11 / 5;
+	mod = 11 % 5;
+	printf("div = %d, mod =%d\n", div, mod);
+
+	div = 11 / -5;
+	mod = 11 % -5;
+	printf("div = %d, mod =%d\n", div, mod);
+
+	div = -11 / -5;
+	mod = -11 % -5;
+	printf("div = %d, mod =%d\n", div, mod); // first operand is negative
+
+	div = -11 / 5;
+	mod = -11 % 5;
+	printf("div = %d, mod =%d\n", div, mod); // first operand is negative
 
 
 	return 0;
