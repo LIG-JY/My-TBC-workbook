0c0dbc92dcf367824048eb88e4c726fe1731579b
unknown <dldlsry053@naver.com>
2022-09-22 04:01:43 +0900

    실습완료

--- Chapter6/Lecture3/lecture3.c
+++ Chapter6/Lecture3/lecture3.c
@@ -4,13 +4,15 @@
 int main()
 {
 	/*
-	while (expression)
+	while (expression) expression이 true값으로 판명이되면, 즉 컴퓨터 내부에서 1이면 아래의 statment를 실행합니다.
 		statement
 	*/
 
 
-
 	/* Common mistakes */
+	int i;
+	while (i++ < 3); // while에 ;붙이는 실수 ㄴㄴ
+	printf("%d", i);
 
 
 
