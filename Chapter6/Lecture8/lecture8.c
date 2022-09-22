231f21b0fa2bcbddd61019d71ab2add5d0b799fa
unknown <dldlsry053@naver.com>
2022-09-23 04:06:57 +0900

    실습완료

--- Chapter6/Lecture8/lecture8.c
+++ Chapter6/Lecture8/lecture8.c
@@ -4,21 +4,28 @@
 int main()
 {
 	// Indefinite loop vs. Counting loop
-
+	while (1)
+	{
+		// do something, 'indefinite' while안에서 무슨 일이 일어날지 모릅니다.
+	}
 
 
 
 	/*
+		while문에서 counting loop 사용하는법?
 		Counting Loop
-		1. Counter initialization
-		2. Counter check
-		3. Counter change
+		1. Counter initialization 변수 초기화
+		2. Counter check 변수 조건문에서 확인
+		3. Counter change 변수 변화(증감)
 	*/
+	int i;
+	 // for (int i = 1; i <= 10; i++) 이렇게 선언하고 초기화를 동시에 해도 괜찮습니다.
+	for (i = 1; i <= 10; i++)  // 초기화, 조건, 변수변화 3가지를 모두 한꺼번에!
+	{
+		print("%d", i);
+	}
 
-
-
-
-
+	// 디버거를 이용하면 가장 왼쪽의 i = 1 expression은 반복문 전체에서 1번 실행됩니다.
 
 
 	return 0;
--- Chapter6/Lecture9/lecture9.c
+++ Chapter6/Lecture9/lecture9.c
@@ -5,13 +5,35 @@ int main()
 {
 	/*
 		for(initialize; test; update)
+		" for문을 실행할 때 initialize expression은 한 번만 실행됩니다. "
+		" test에서 False가 아니면 아래의 statement를 실행합니다. "
+		" updat하고 다시 test가 실행됩니다. " 
 			statement
 	*/
 
+	for (int i = 10; i > 0; i--);
+		// do something
 
+	for (int i = 10; i < 100; i = i + 8);
+		// do something
 
+	for (char c = 'A'; c <= 'Z'; c++);  // 내부적으로 ASCII
+		// do something
 
+	for (int i = 0; i * i < 10; i++);
+		// do something
+	
+	for (int x = 1, y = 5; y <= 20; y = (++x * 3) + 10);  // 같은 자료형의 변수는 여러개 사용할 수 있다.
+		// do something
 
+	for (double d = 100.0; d > 300; d = d * 1.1);  // 정수형과 실수형 비교할 때 주의점 != 같이 일치를 쓰면 어려울 수 있다.
+		// do something
 
+	for (;;);
+	//  while(1) 이랑 동일합니다.
+
+	int i = 0;
+	for (printf("Let's go! \n"); i != 7; scanf("%d", &i));
+	// update를 scanf로 받습니다...
 	return 0;
 }
