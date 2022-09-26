67f3870996ff7650029dad8160f9d5b37c17ca38
LIG_JY <dldlsry053@naver.com>
2022-09-26 17:22:17 +0900

    실습완료

--- Chapter6/Lecture15_1/lecture15_1.c
+++ Chapter6/Lecture15_1/lecture15_1.c
@@ -1,18 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define NUM_ROWS	5
+#define NUM_ROWS	5  // c언어스럽게 define을 이용합니다.
 #define FIRST_CHAR	'A'
 #define LAST_CHAR   'K'
 
 int main()
 {
-	//int r;// row loop
-	//int c;// character loop
-
-
-
-
+	
+	for(int i = 1; i <= NUM_ROWS; i++)  // outer row loop
+	{
+		for(char j = FIRST_CHAR; j <= LAST_CHAR; j++)  // inner character loop
+		{
+			printf("%c ", j);	
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
--- Chapter6/Lecture15_3/lecture15_3.c
+++ Chapter6/Lecture15_3/lecture15_3.c
@@ -1,14 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//#define NUM_ROWS 12
+#define NUM_ROWS	12
 #define FIRST_CHAR	'A'
-//#define LAST_CHAR   'K'
+#define LAST_CHAR   'K'
 
 int main()
 {
-
-
+	int r;// row loop
+	int c;// character loop
+	for(r = 0; r < NUM_ROWS; ++r)
+	{
+		for(c = FIRST_CHAR + r; c <= LAST_CHAR + 1; c++)
+		{
+			printf("%c ", c);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
