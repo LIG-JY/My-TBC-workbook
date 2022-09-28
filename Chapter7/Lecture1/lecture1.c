c0b6788c7c26b615705c8df50e06a53d91ca97e8
LIG_JY <dldlsry053@naver.com>
2022-09-28 23:31:00 +0900

    실습완료

--- Chapter7/Lecture1/lecture1.c
+++ Chapter7/Lecture1/lecture1.c
@@ -3,11 +3,15 @@
 
 int main()
 {
-
+	int number;
 
 	printf("Input a positive integer : ");
+	scanf("%d", &number);
 
-
+	if (number % 2 == 0)
+		printf("Even");
+	else
+		printf("Odd");
 
 
 	//TODO: print even or odd
