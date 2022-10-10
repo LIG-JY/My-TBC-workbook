f14aea8bdb32be71fdbddaa7c1c46b88da35474e
이재연 <dldlsry053@naver.com>
2022-10-10 21:32:56 +0900

    Update lecture10.c
    실습완료

--- Chapter7/Lecture10/lecture10.c
+++ Chapter7/Lecture10/lecture10.c
@@ -10,6 +10,15 @@ int main()
 
 
 	/* for and continue */
+	for (int count = 0; count < 10; ++count) // getchar 입력할 때 buffer 확인
+	{
+		int c = getchar();
+		if (c == 'a')
+		{
+			continue;
+		}
+		putchar(c);
+	}
 
 
 	/* continue as a placeholder */
