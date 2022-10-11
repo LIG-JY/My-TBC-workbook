3ffce1a874c04ff59840aa67e43b78edd419df70
이재연 <dldlsry053@naver.com>
2022-10-11 22:11:52 +0900

    실습완료

--- Chapter8/Lecture2/lecture2.c
+++ Chapter8/Lecture2/lecture2.c
@@ -1,10 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+
 int main()
 {
-
-
+	while(1)
+	{
+		c = getchar();
+		printf("%d\n", c);
+		if ( c == EOF)
+		{
+			break;
+		}
+	}
 
 	return 0;
 }
