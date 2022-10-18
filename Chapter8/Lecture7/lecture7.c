22bfb5d2941b92fd32c29ffc4831bc342c64dd0e
unknown <dldlsry053@naver.com>
2022-10-19 02:48:37 +0900

    실습완료

--- Chapter8/Lecture7/lecture7.c
+++ Chapter8/Lecture7/lecture7.c
@@ -8,18 +8,24 @@ int main()
 		Hello 123 3.14
 	*/
 
+	char str[255];
+	int i, i2;
+	double d;
 
-
-
+	scanf("%s %d %lf\n", &str, &i, &d);
+	printf("%s %d %f\n", str, i, d);
 
 	//// or (see the difference)
 
+	scanf("%s %d %d\n", &str, &i, &i2);
+	printf("%s %d %d\n", str, i, i2);
+
+	//// or (see the difference)
 	
-
-
-	//// or (see the difference)
-
-
+	char c;
+	while ((getchar()) != '\n')
+		putchar(c);
+	printf("\n");
 
 	return 0;
 }
