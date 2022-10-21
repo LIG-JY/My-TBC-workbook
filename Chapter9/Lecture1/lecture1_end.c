0e89a769a38e35d818ef204856a913f3bbb2b006
unknown <dldlsry053@naver.com>
2022-10-22 03:25:53 +0900

    실습완료

--- Chapter9/Lecture1/lecture1_end.c
+++ Chapter9/Lecture1/lecture1_end.c
@@ -3,23 +3,37 @@
 #include <string.h>	// strlen()
 #include <stdbool.h>
 
-#define WIDTH	25
+#define WIDTH	60
 #define NAME	"Your name"
 #define ADDRESS "City, Citizenship"
+#define Aw "Yahoo"
 
-void print_multiple_chars(char c, int n_stars, bool print_newline)
+void print_multiple_chars(char c, int n_stars, bool print_newline) // 정의(선언과 정의의 차이점 생각하기)
 {
-
+	for (int i = 0; i < n_stars; ++i)
+		printf("%c", c);   // putchar(c);
+	if (print_newline)
+	{
+		printf("\n");
+	}
 }
+
 
 void print_centered_str(char str[])
 {
-
+	int n_blanks = 0;
+	n_blanks = (WIDTH - strlen(str)) / 2;
+	print_multiple_chars(' ', n_blanks, false);
+	printf("%s\n", str);
 }
 
 int main()
 {
-
-
+	print_multiple_chars('@', WIDTH, 1);
+	print_centered_str(NAME);
+	print_centered_str(ADDRESS);
+	print_centered_str(Aw);
+	print_multiple_chars('@', WIDTH, 1);
 	return 0;
 }
+
