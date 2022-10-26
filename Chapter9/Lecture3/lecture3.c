7bfd443530d2a8c1a3c0afbc86273832a9fb6294
이재연 <dldlsry053@naver.com>
2022-10-26 22:55:40 +0900

    실습완료

--- Chapter9/Lecture3/lecture3.c
+++ Chapter9/Lecture3/lecture3.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int int_min(int, int);
+int int_min(int, int); // 반환 자료형이 Int
 
 int main()
 {
@@ -11,7 +11,7 @@ int main()
 	{
 		printf("Input two integers : ");
 		
-		if (scanf("%d %d", &i1, &i2) != 2) break;
+		if (scanf("%d %d", &i1, &i2) != 2) break; // 정수 자료형이 2개 입력되지 않으면 break
 		
 		int lesser = int_min(i1, i2);
 		//int lesser = min;
@@ -27,7 +27,10 @@ int main()
 }
 
 int int_min(int i, int j)
-//int_min(int i, int j)
+
+// 반환 자료형이 int 문법상 생략할 수 있습니다. 
+// 즉 컴파일러가 반환 자료형이 없다면 int라고 간주합니다. 하지만 생략 안 하는 것을 추천합니다.
+// int_min(int i, int j)
 {
 	int min;
 
@@ -40,15 +43,15 @@ int int_min(int i, int j)
 
 	//return (i < j) ? i : j;
 
-	//return (float)min;
+	//return (float)min; -> int로 리턴하지 않고, 강제로 float로 형변환해서 return하는 경우
 
 	//if (i < j)
 	//	return i;
 	//else
 	//	return j;
 
-	//printf("ERROR");
-	//exit(1);
+	//printf("ERROR"); -> 의도적으로 실행되지 않는 코드를 적어주는 것
+	//exit(1); -> 일어나면 안 될 일이 일어나고 있다!!!
 
 	//return; // return with no value
 }
