bbf51769966e7e18e49b966b5625f5cbcefa6370
unknown <dldlsry053@naver.com>
2022-11-02 03:48:48 +0900

    실습오나료

--- Chapter9/Lecture8/lecture8.c
+++ Chapter9/Lecture8/lecture8.c
@@ -12,22 +12,29 @@ long recursive_factorial(int n);
 
 int main()
 {
-
-
+	printf("%ld\n", loop_factorial(5));
+	printf("%ld\n", recursive_factorial(5));
 	return 0;
 }
 
-//long loop_factorial(int n)
-//{
-//	long ans;
-//
-//
-//
-//
-//	return ans;
-//}
+long loop_factorial(int n)
+{
+	long ans;
+	ans = 1;
+	while (n > 0)
+	{
+		ans *= n;
+		n -= 1;
+	}
+	return ans;
+}
 
-//long recursive_factorial(int n)
-//{
-//	
-//}
+long recursive_factorial(int n)
+{
+	if (n > 1)
+	{
+		return n * recursive_factorial(n - 1); // tail(end) recursion
+	}
+	else
+		return 1;
+}
