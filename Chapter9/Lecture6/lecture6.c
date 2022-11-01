5ba985a2e0cea8ce5ad62330c9b8d2d65dd49428
이재연 <dldlsry053@naver.com>
2022-11-01 23:59:32 +0900

    실습완료

--- Chapter9/Lecture6/lecture6.c
+++ Chapter9/Lecture6/lecture6.c
@@ -5,13 +5,18 @@ void my_func(int);
 
 int main()
 {
-
-
+	my_func(1);
 	return 0;
 }
 
 void my_func(int n)
 {
+	printf("Level %d, address of variable n = %p\n", n, &n);
 
+	if (n <=4 ) // 종료조건
+	{
+			my_func(n + 1); // 재귀호출
+	}
 
+	printf("Level %d, address of variable n = %p\n", n, &n); // stack에서 돌아오는 경로 확인
 }
