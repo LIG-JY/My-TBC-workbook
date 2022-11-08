e3084a3fa7af834de291eb1b776d620312354f94
unknown <dldlsry053@naver.com>
2022-11-09 04:31:44 +0900

    11.8실습완료

--- Chapter9/Lecture9/lecture9.c
+++ Chapter9/Lecture9/lecture9.c
@@ -15,24 +15,40 @@ void print_binary_loop(unsigned long n);
 int main()
 {
 
-
-
-
+	print_binary(10);
+	printf("\n");
+	print_binary_loop(10);
+	
 	return 0;
 }
 
 //Note: printing order is reversed!
 void print_binary_loop(unsigned long num)
-{
+{	
+	while (1)
+	{
+		int quotient = num / 2; // 정수로 형변환 해서 소수점 이하를 버린다.
+		int remainder = num % 2;
 
+		printf("%d", remainder); // remainder is 0 or 1
 
+		num = quotient;  // 출력 순서를 바꾸기 위해서는? 배열이 필요합니다.
+
+		if (num == 0)
+		{
+			break;
+		}
+	}
 	printf("\n");
 }
 
 void print_binary(unsigned long num)
 {
-
-
-
+	int quotient = num / 2;
+	if (quotient > 0)
+	{
+		print_binary(quotient); // print만 하면 되기 때문에, return이 필요없는 것을 생각합시당.
+	}
+	printf("%d", num % 2); // stack 개념
 	return;
 }
