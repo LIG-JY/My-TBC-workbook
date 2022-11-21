19614cfba30b859b2179242078d1dc9c9d5295ed
이재연 <dldlsry053@naver.com>
2022-11-21 23:41:44 +0900

    11.21

--- Chapter10/Lecture6/lecture6.c
+++ Chapter10/Lecture6/lecture6.c
@@ -4,6 +4,7 @@
 #define MONTHS 12
 #define YEARS 3
 
+
 int main()
 {
 	double year2016[MONTHS] = { -3.2, 0.2, 7.0, 14.1, 19.6, 23.6, 26.2, 28.0, 23.1, 16.1, 6.8, 1.2 };
@@ -18,26 +19,58 @@ int main()
 	*/
 
 
-
 	/* Step 2 */
+	double years[YEARS][MONTHS] = {{ -3.2, 0.2, 7.0, 14.1, 19.6, 23.6, 26.2, 28.0, 23.1, 16.1, 6.8, 1.2 },
+								   { -1.8, -0.2, 6.3, 13.9, 19.5, 23.3, 26.9, 25.9, 22.1, 16.4, 5.6, -1.9 },
+								   { -4.0, -1.6, 8.1, 13.0, 18.2, 23.1, 27.8, 28.8, 21.5, 13.1, 7.8, -0.6 }};
 	printf("[Temperature Data]\n");
+	printf("Year index : \t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\n");
+	for (int i=0; i<YEARS; ++i)
+	{
+		printf("Year %d     : ",i );
+		for (int j=0; j<MONTHS; ++j)
+		{
+			printf("\t%.1lf", years[i][j]);
+		}
+		printf("\n");
+	}
+
 
 	
 
 	/* Step 3 */
-	printf("[Yearly average temperatures of 3 years]\n");
-
-
 	printf("\n");
+	printf("[Yearly average temperatures of 3 years]\n");
+	for (int i=0; i<YEARS; ++i)
+	{
+		double year_avg = 0.0;
+		for (int j = 0; j<MONTHS; ++j)
+		{
+			year_avg += years[i][j];
+		}
+		printf("Year %d : average temperature = %.1lf\n",i ,year_avg/12);
+	}
+	
 
 	/* Step 4 */
+	printf("\n");
 	printf("[Monthly average temperatures for 3 years]\n");
 	printf("Year index : ");
-
-	
+	for (int i=0; i < MONTHS; ++i)
+	{
+		printf("\t%d", i+1);
+	}
+	printf("\n");
 	printf("Avg temps  : ");
-
-
+	for (int i=0; i<MONTHS; ++i)
+	{
+		double month_avg = 0.0;
+		for (int j=0; j<YEARS; ++j)
+		{
+			month_avg += years[j][i];
+		}
+		printf("\t%.1lf", month_avg/3);
+	}
 
 	return 0;
 }
