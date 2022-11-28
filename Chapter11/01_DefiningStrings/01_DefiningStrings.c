b9d12b3f280a614c2a6c4faa8a889631c60994a3
unknown <dldlsry053@naver.com>
2022-11-28 22:15:47 +0900

    11.28

--- Chapter11/01_DefiningStrings/01_DefiningStrings.c
+++ Chapter11/01_DefiningStrings/01_DefiningStrings.c
@@ -1,16 +1,69 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
+#define MESSEGE "A symbolic string contant"
+#define MAXLENGTH 81  // Symbolic constant
 
 int main()
 {
+	char words[MAXLENGTH] = "A string is an array";
+	const char* pt1 = "A pointer to a string.";  // 문자열이 저장된 긴 메모리 공간의 첫번째 주소를 담는 포인터
+	/*위의 두 코드의 차이를 확인해본다. 첫번째 char의 배열로 선언하는 경우 words라는 자기의 메모리가 존재한다.
+	하지만 두번째 코드의 경우 '어딘가'에 저장되어있는 문자열의 첫번째 주소를 가리키게 된다.*/
 
+	puts("Puts() adds a newline at the end:");  // puts() add \n at the end
+	puts(MESSEGE);
+	puts(words);
+	puts(pt1);
 
+	words[3] = 'p';  // 배열이라서 교체가 정상적으로 작동합니다.
+	puts(words);
+	/*char* pt1 = "A pointer to a string.";*/
+	/*pt1[3] = 'h';*/  // 수정할 수 없는 lvalue, 교체할 수 없다. const 때문입니다. 하지만 cosnt를 땐다면?
 
-	printf("\" To be, or not to be\" Hamlet said.\n");
+	char greeting[50] = "Hello" "kcoW" "yeah";  // 중간에 따옴표 넣어도 따옴표 다 생략해서 출력됩니다.
+	puts(greeting);
+	printf("\" To be, or not to be\" Hamlet said.\n");   // 겹따옴표 자체를 출력할 때는 escape sequence
 
+	printf("%s, %p, %c\n", "WE", "ARE", *"excellent programming");
+	// 1) prtinf에서 %s는 문자열의 format specifier이다.
+	// 2) printf에서 %p는 문자열에서 첫번째 char(문자)의 주소를 가리킨다. A의 첫번재 주소를 담고 있다.
+	// 3) excellent~~~ 라는 문자열에서 첫번째 char(문자)e의 주소를 가리킨다. e의 주소에서 redirection하면 e가 나온다.
 
+	const char m1[15] = "Love you!";
+	for (int i = 0; i < 15; ++i)
+	{
+		printf("%d ", (int)m1[i]);  // Note the null character
+	}
+	printf("\n");
+
+	const char m2[15] = { 'L','o','v','e','y','o','u','!','\0' };
+	for (int i = 0; i < 15; ++i)
+	{
+		printf("%d ", (int)m2[i]);  // Note the null character, 마지막에 Null character 빼도 컴파일러가 잘 넣어줍니다.
+	}
+	printf("\n");
+
+	const char m3[] = "Love you, too!";
+	for (int i = 0; i < 15; ++i)
+	{
+		printf("%d ", (int)m3[i]);  // Note the null character
+	}
+	printf("\n");
+
+	char cookies[2 * sizeof(int)] = { 'A', };
+	puts(cookies);  // Null character 출력 X
+	for (int i = 0; i < 2 * sizeof(int); ++i)
+	{
+		printf("%d ", (int)cookies[i]);  // Note the null character
+	}
+	printf("\n");
+	
+	char truth[10] = "Truths is ";
+	if (truth == &truth[0]) puts("true!");   // 배열의 이름은 포인터다. 첫번째 원소의 가장 첫번째 주소를 담고 있다.
+	if (*truth == 'T') puts("true!");  // 배열의 이름이 포인터이기 때문에 indirection이 가능하다. 첫번째 원소를 찾아간다.
+	if (*(truth + 1) == truth[1]) puts("true!"); // 배열의 이름이 포인터이기 때문에, 포인터는 연산이 가능하다. 인덱스 한 칸씩 이동하는 것과 똑같다.
+	if (truth[1] == 'r') puts("true!"); // 배열의 이름에서 subscripting 기능을 사용했다. 배열의 성질
 
 	return 0;
 }
--- Chapter11/02_ArraysVsPointers/02_ArraysVsPointers.c
+++ Chapter11/02_ArraysVsPointers/02_ArraysVsPointers.c
@@ -4,34 +4,118 @@
 
 void test_function()
 {
-
+	int j;
+	printf("Stack high \t%llu \n", (unsigned long long) & j);
 }
 
 int main()
 {
 	/* Array Versus Pointer */
+	const char* pt2 = "I am a string.";
+	const char* pt3 = "I am a string.";
+	const char* pt4 = "I am a string!!!";
+	const char ar1[] = "I am a string.";
+	const char ar2[] = "I am a string.";
+	const char ar3[] = "I am a string!!!";
 
+	printf("rodata(read only data) low \t%llu %llu %llu %llu\n", (unsigned long long) pt2,
+		(unsigned long long) pt3, (unsigned long long) pt4,
+		(unsigned long long) "I am a string.");
 
+	printf("Stack high \t%llu %llu %llu \n", (unsigned long long) ar1,
+		(unsigned long long) ar2, (unsigned long long) ar3);
+
+	// 위의 출력문에서 stack과 rodat의 주소 배치를 각각 확인해야함. 각각 분류끼리 가까운 주소를 사용한다.
+	// 첫번째 printf문에서 pt2, pt3은 같은 rodata의 주소를 담고 있습니다. 그리고 "I am a string."을 직접 출력할 때도 같은 값이 나옵니다.
+	// >>> 이유 질문?
+	// 두번째 prinf문에서 배열은 각각 메모리를 확보하고 있기 때문에 주소가 다르다.
 
 	/* Memory address check */
 
-	/* Local variables */
 
+
+	/* Local variables */
+	printf("Stack high \t%llu %llu %llu \n", (unsigned long long) &pt2,
+		(unsigned long long) & pt3, (unsigned long long) & pt4);
+	/*포인터 변수도 변수니까 주소를 가진다.
+	스택에 올라갑니다. 지역변수*/
+	int i;
+	printf("Stack high \t%llu \n", (unsigned long long) & i);
+	// i도 초기화되지 않았지만, 지역변수이고 스택에 올라갑니다.
 
 
 	/* Local variable in a function*/
 	test_function();
 
 	/* Dynamic allocation */
-	
+	char* p5 = (char*)malloc(sizeof(char) * 100);
+	printf("heap middle \t%llu \n", (unsigned long long) p5);
+
+	char* p6 = (char*)malloc(sizeof(char) * 100);
+	printf("heap middle \t%llu \n", (unsigned long long) p6);
+	// 동적할당은 크기를 예상할 수 없으니까 heap으로 stack과 비교해서 생각하자.
 
 	/* Array Versus Pointer (continued) */
 
-	
+	//ar1++; // error; 배열의 이름과 포인터의 차이점을 알 수 있다. 배열의 이름에 증감연산자 사용불가
+	pt2++; // 포인터는 증감연산자를 사용할 수 있다.
+	puts(pt2); // "I am a string."에서 증감연산자 때문에 한칸 이동해서  " am a string."이 출력된다.
 
 	/* Array and Pointer Differences */
-		
+	char heart[] = "I love kelly";
+	const char* head = "I love kelly";
 
+	for (int i = 0; i < 6; ++i)
+	{
+		putchar(heart[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 6; ++i)
+	{
+		putchar(head[i]);
+	}
+	printf("\n");
+
+	// pointer addition
+	for (int i = 0; i < 6; ++i)
+		putchar((*(heart + i)));
+	printf("\n");
+
+	for (int i = 0; i < 6; ++i)
+		putchar((*(head + i)));
+	printf("\n");
+
+	/*문자열 전부를 출력하는 코드*/
+	while (*(head) != '\0')  // 포인터로 indirection 했을 때 Nullchar이면 문자가 끝이니 종료
+	{
+		putchar(*(head++));  // 포인터 이동
+	}
+
+	head = heart; // ok, 포인터가 가리키는 주소를 변경, 배열의 이름은 포인터와 호완된다.
+	// heart = head; 반대로는 안 된다.
+
+	// Cannot change heart. Can change the elements of heart
+	heart[7] = 'H';  // 배열의 이름은 바꿀 수 없다. 증감연산자를 사용할 수 없듯이.
+	*(heart + 7) = 'K';  // 배열의 이름이 포인터 변수와 호완이 되기 때문에 산술연산이 가능하다.
+
+	putchar('\n');
+
+	char* word = "Google";
+	/*word[2] = 'k';*/  // Runtime error, 포인터가 가리키는 literal은data segment에 있다. 따라서 X
+	// const char* word = "Google"; 을 권합니다. C언어 refernecing
+
+	const char* str1 = "a Whole new world";
+	const char* copy;
+
+	copy = str1;  // 포인터의 주소를 복사했다. data segment의 문자열의 주소만 복사했다.
+
+	printf("%s %p %p\n", str1, str1, &str1);
+	printf("%s %p %p\n", copy, copy, &copy);
+	// 첫번째는 버퍼로 printf에서 문자열 출력, 두번째는 str1, copy라는 포인터 변수가 담고있는 주소값 (data segment에 올라간 주소)
+	// 세번째는 이 포인터 변수의 주소값, Stack에 올라갑니다.
+	// 2개의 출력문이 동일함을 인지하자. 얕은 복사	
 
 	return 0;
 }
+
