0c03bcd59631da21396d19e48e847082b6206496
이재연 <dldlsry053@naver.com>
2022-12-17 05:05:37 +0900

    12.16

--- Chapter14/06_PointersToStructures/main.c
+++ Chapter14/06_PointersToStructures/main.c
@@ -14,12 +14,22 @@ struct friend {
 
 int main(void)
 {
+	struct friend my_friend[2] = {
+		{{"Ariana", "Grande"}, "1234-1234"},
+		{{"Taylor", "Swift"}, "8080-5555"},
+	};
 
+	struct friend* girl_Freind;
+	girl_Freind = &my_friend[0];
 
+	printf("size of struct freind is %zd\n", sizeof(struct friend));
+	printf("address of pointer girl_Freind is %lld\n and member of member is %s\n",
+		(long long)girl_Freind, girl_Freind->full_name.given);
 
+	girl_Freind++;  // 포인터연산
 
-
-	// . has higher precedence than *
+	printf("address of pointer girl_Freind is %lld\n and member of member is %s\n",
+		(long long)girl_Freind, girl_Freind->full_name.given);
 
 	return 0;
 }
