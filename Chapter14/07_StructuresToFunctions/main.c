a318b6d706f13f3530b3f8e70fb209795d16d8f0
이재연 <dldlsry053@naver.com>
2022-12-19 00:51:06 +0900

    12.18

--- Chapter14/07_StructuresToFunctions/main.c
+++ Chapter14/07_StructuresToFunctions/main.c
@@ -3,20 +3,30 @@
 #include <string.h>
 #define FUNDLEN 50
 
-//struct fortune {
-//
-//};
+struct fortune {
+	char bank_name[FUNDLEN];
+	double bank_saving;
+	char fund_name[FUNDLEN];
+	double fund_invest;
+};
 
-double sum(struct fortune *my_fortune);
+double sum(const struct fortune* client);
 
 int main()
 {
+	struct fortune client_1 = {
+		.bank_name = "Doichibank",
+		.bank_saving = 50000.0,
+		.fund_invest = 30000.0,
+		.fund_name = "JP"
+	};
 
+	printf("Total:\t%.2lf\n", sum(&client_1));
 
 	return 0;
 }
 
-//double sum(const struct fortune *mf)	//TOOD: try pointers
-//{
-//
-//}
+double sum(const struct fortune* client)
+{
+	return client->bank_saving + client->fund_invest;
+}
