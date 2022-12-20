82f531a9b60328ed2616e9196dc21498cd6e1af4
이재연 <dldlsry053@naver.com>
2022-12-21 03:52:58 +0900

    12.20

--- Chapter14/10_CompoundLiterals/main.c
+++ Chapter14/10_CompoundLiterals/main.c
@@ -19,15 +19,15 @@ struct rectangle
 	double height;
 };
 
-//double rect_area(struct rectangle r)
-//{
-//
-//}
-//
-//double rect_area_ptr(struct rectangle * r)
-//{
-//
-//}
+double rect_area(struct rectangle r)
+{
+	return r.height * r.width;
+}
+
+double rect_area_ptr(struct rectangle * r)
+{
+	return r->height * r->width;
+}
 
 int main()
 {
@@ -37,12 +37,26 @@ int main()
 		Compound literals
 		- Temporary structure values
 	*/
-	
+
 
 	//book_to_read = { "Alice in Wonderland", "Lewis Carroll", 20.3f };//Error
+	strcpy(book_to_read.title, "A whole new world");
+	strcpy(book_to_read.author, "Aladin");
+	book_to_read.price = 20.3f;
 
+	struct book book_to_read2 = { "Alice in Wonderland", "Lewis Carroll", 20.3f };
+	book_to_read = book_to_read2;
 
+	book_to_read = (struct book) { "Alice in Wonderland", "Lewis Carroll", 20.3f };
 
+	struct rectangle rec1 = { 1.0 ,2.0 };
+	double area = rect_area(rec1);
+	printf("%lf\n", area);
+	area = rect_area((struct rectangle) { 2.0, 3.0 });
+	printf("%lf\n" , area);
+
+	area = rect_area_ptr(&(struct rectangle) { .height = 5.0, .width = 6.0 });
+	printf("%lf\n", area);
 
 	return 0;
 }
