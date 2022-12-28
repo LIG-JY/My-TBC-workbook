1aba56c7149b2eb96ca10bee82cafd414e66ff49
이재연 <dldlsry053@naver.com>
2022-12-29 06:12:00 +0900

    12.28

--- Chapter14/25_ArrayOfFunctionPointers/main.c
+++ Chapter14/25_ArrayOfFunctionPointers/main.c
@@ -11,7 +11,10 @@ void Transpose(char* str);//TODO: add a new menu
 
 int main()
 {
+	char input[100];
 
+	while (scanf("%[^\n]%*c", input) != 1)
+		printf("Please try agian!!!\n");
 
 	while (true)
 	{
@@ -19,11 +22,39 @@ int main()
 		printf("u) to upper\n");
 		printf("l) to lower\n");
 		printf("t) to transpose\n");//TODO: add a new menu
+		
+		char c;
+		while (scanf("%c%*[^\n]%*c", &c) != 1)
+			printf("Please try agian.\n");
 
+		typedef void (*FUCKPTR)(char*);
+		FUCKPTR options[] = { ToUpper , ToLower ,Transpose };
+		char operation[] = { 'u', 'l', 't' };
 
+		void (*fucptr)(char*);
+		for (int i = 0; i < sizeof(operation) / sizeof(char); i++)
+		{
+			if (c == operation[i])
+			{
+				fucptr = options[i];
+				fucptr(input);
+			}
+		}
+
+		/*switch (c) {
+		case 'u': fucptr = ToUpper; fucptr(input);  break;
+			
+		case 'l': fucptr = &ToLower; fucptr(input); break;
+
+		case 't': fucptr = options[2]; fucptr(input); break;
+
+		default: break;
+		}*/
+		
+		break;
 	}
 
-	
+	printf("%s", input);
 
 	return 0;
 }
@@ -35,15 +66,34 @@ void update_string(char* str, int(*pf)(int))
 
 void ToUpper(char* str)
 {
-
+	while (*str)
+	{
+		*str = toupper(*str);
+		str++;
+	}
 }
 
 void ToLower(char* str)
 {
-
+	while (*str)
+	{
+		*str = tolower(*str);
+		str++;
+	}
 }
 
 void Transpose(char* str)
 {
-
+	while (*str)
+	{
+		if (islower(*str))
+		{
+			*str = toupper(*str);
+		}
+		else
+		{
+			*str = tolower(*str);
+		}
+		str++;
+	}
 }
